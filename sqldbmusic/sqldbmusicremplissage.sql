0c97edac1fc0ebd7401015961cfb41d101cac610
Lokionn <122351015+Lokionn@users.noreply.github.com>
2023-01-20 17:10:08 +0100

    Creation data base sql
    Vous executez dabord la creation, ensuite l'alter, puis le remplissage
    Pas besoin de le faire tout de suite cest juste pour que joublie pas de mettre ce que je fais sur git
    Bon weekend <3

---
+++ sqldbmusic/sqldbmusicremplissage.sql
@@ -0,0 +1,704 @@
+use bd_music_project;
+
+INSERT INTO 
+ users 
+(
+Users_first_name, 
+Users_last_name, 
+Users_Adress,
+Users_Email,
+Users_bio,
+Users_City, 
+Users_Zip
+)
+VALUES
+(
+
+'Logan',
+'Lassere',
+'56 rue Marx Dormoy',
+'logan1998@hotmail.fr',
+'guitariste depuis peu je cherche a faire des rencontres pour pratiquer et devenir meilleur',
+'Grenoble',
+'38100'
+)
+;
+INSERT INTO 
+ users 
+(
+Users_first_name, 
+Users_last_name, 
+Users_Adress,
+Users_Email,
+Users_bio,
+Users_City, 
+Users_Zip
+)
+VALUES
+(
+
+'Marion',
+'Choupinax',
+'27 rue de lespoir',
+'marion@hotmail.fr',
+'pianiste depuis peu je cherche a faire des rencontres pour pratiquer et devenir meilleur',
+'Toulouse',
+'31000'
+)
+;
+INSERT INTO 
+ users 
+(
+Users_first_name, 
+Users_last_name, 
+Users_Adress,
+Users_Email,
+Users_bio,
+Users_City, 
+Users_Zip
+)
+VALUES
+(
+
+'Frédéric',
+'Dupont',
+'45 rue voltaire',
+'fdupont@hotmail.fr',
+'Trompetiste depuis peu je cherche a faire des rencontres pour pratiquer et devenir meilleur',
+'La Courneuve',
+'93120'
+)
+;
+INSERT INTO 
+ users 
+(
+Users_first_name, 
+Users_last_name, 
+Users_Adress,
+Users_Email,
+Users_bio,
+Users_City, 
+Users_Zip
+)
+VALUES
+(
+'Fatimah',
+'Baker',
+'29 rue montrouge',
+'fbeks@hotmail.fr',
+'violoniste depuis peu je cherche a faire des rencontres pour pratiquer et devenir meilleur',
+'Montpellier',
+'34000'
+)
+;
+INSERT INTO 
+ users 
+(
+Users_first_name, 
+Users_last_name, 
+Users_Adress,
+Users_Email,
+Users_bio,
+Users_City, 
+Users_Zip
+)
+VALUES
+(
+'Rayan',
+'Rabehi',
+'12 rue rateau',
+'r.rabehi@hotmail.fr',
+'violoncelliste depuis peu je cherche a faire des rencontres pour pratiquer et devenir meilleur',
+'Saint Denis',
+'93700'
+)
+;
+INSERT INTO 
+ users 
+(
+Users_first_name, 
+Users_last_name, 
+Users_Adress,
+Users_Email,
+Users_bio,
+Users_City, 
+Users_Zip
+)
+VALUES
+(
+'Feriel',
+'Hamadene',
+'2bis rue des petits fours',
+'f.hamdne@hotmail.fr',
+'percursionniste depuis peu je cherche a faire des rencontres pour pratiquer et devenir meilleur',
+'Grenoble',
+'38000'
+)
+;
+INSERT INTO 
+ users 
+(
+Users_first_name, 
+Users_last_name, 
+Users_Adress,
+Users_Email,
+Users_bio,
+Users_City, 
+Users_Zip
+)
+VALUES
+(
+'Fernand',
+'Lux',
+'3 rue des amours',
+'f.lux@hotmail.fr',
+'percursionniste depuis peu je cherche a faire des rencontres pour pratiquer et devenir meilleur',
+'Grenoble',
+'38000'
+)
+;
+INSERT INTO 
+ users 
+(
+Users_first_name, 
+Users_last_name, 
+Users_Adress,
+Users_Email,
+Users_bio,
+Users_City, 
+Users_Zip
+)
+VALUES
+(
+'Killyan',
+'Gruyere',
+'56 cours berriat',
+'killyan2000@hotmail.fr',
+'drummer depuis peu je cherche a faire des rencontres pour pratiquer et devenir meilleur',
+'Grenoble',
+'38000'
+)
+;
+INSERT INTO 
+ users 
+(
+Users_first_name, 
+Users_last_name, 
+Users_Adress,
+Users_Email,
+Users_bio,
+Users_City, 
+Users_Zip
+)
+VALUES
+(
+'Mathéo',
+'Moreau',
+'3 rue thiers',
+'m.moreau@hotmail.fr',
+'bassistee depuis peu je cherche a faire des rencontres pour pratiquer et devenir meilleur',
+'Grenoble',
+'38000'
+)
+;
+INSERT INTO 
+ users 
+(
+Users_first_name, 
+Users_last_name, 
+Users_Adress,
+Users_Email,
+Users_bio,
+Users_City, 
+Users_Zip
+)
+VALUES
+(
+'Jeya',
+'Niska',
+'4 rue des arts',
+'j.Nisks@hotmail.fr',
+'guitariste depuis peu je cherche a faire des rencontres pour pratiquer et devenir meilleur',
+'Grenoble',
+'38000'
+)
+;
+
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+)
+VALUES
+(
+'accordéon',
+'vent'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'batterie',
+'percussion'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'banjo',
+'cordes'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'cajon',
+'percussion'
+)
+;
+
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'cithare',
+'corde pincées'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'clarinette',
+'bois'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'contrebasse',
+'cordes'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'Dixianqin',
+'corde'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'Voix',
+'chant'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'dizi',
+'bois'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'djembé',
+'percussion'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'euphonium',
+'cuivre'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'flûte',
+'vent'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'flûte de pan',
+'bois'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'flûte traversière',
+'bois'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'guitare',
+'corde'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'harpe',
+'corde'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'sumagoto',
+'corde'
+)
+;
+
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'junjung ',
+'percussion'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'luth',
+'corde'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'lyre',
+'corde'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'mandoline',
+'corde'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'ocarina',
+'vent'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'orgue',
+'vent'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'piano',
+'corde'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'saxophone',
+'cuivre'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'tam-tam',
+'percussion'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'timbales',
+'percussion'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'Musique assistée par ordinateur',
+'analogique'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'trompette',
+'cuivre'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'trombonne',
+'cuivre'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'ukulélé',
+'corde'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'violon',
+'corde'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'violoncelle',
+'corde'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'xylophone',
+'percussion'
+)
+;
+INSERT INTO 
+ musicinstruments 
+(
+Instr_name, 
+Instr_type 
+
+)
+VALUES
+(
+'yuka',
+'percussion'
+)
+;
+
+INSERT INTO 
+ genre_music 
+(
+Genre_name
+)
+VALUES
+('classique'),('pop'),('variété'),('electronique'),('rock'),('metal'),('rap'),('jazz'),('musique electronique'),('autre')
+;
